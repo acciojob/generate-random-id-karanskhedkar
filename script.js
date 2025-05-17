71317a165aaf7c5bd8824c1c4a85482d08b77340
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-17 22:08:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function makeid(l) {
-  // write your code here
+  const chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const nums="1234567890";
+  let id="";
+  for(let i=0;i<l;i++){
+    const index=Math.ceil(Math.random()*10);
+    if(index%2)
+        id+=nums[index];
+    else
+        id+=chars[index];
+  }
+  return id;
 }
 
 // Do not change the code below.
